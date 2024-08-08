8cdada1c08fb5ba46bd59f0a2b98d43a2007f2a8
Zhyldyz Baiseitova <baiseitovazhyldyz4@gmail.com>
2024-08-08 07:45:23 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("C:\Users\jikab\Desktop\CICD\gitlab-cicd-431608-b129d9fbffc9.json")  # Путь к файлу с ключом сервисного аккаунта
+  credentials = file("/Users/jikab/Desktop/CICD/gitlab-cicd-431608-b129d9fbffc9.json")  # Путь к файлу с ключом сервисного аккаунта
   project     = "gitlab-cicd-431608"                           # ID проекта в GCP
   region      = "us-central1"                                   
 }
@@ -22,4 +22,3 @@ resource "google_compute_subnetwork" "default" {
   network        = google_compute_network.default.id
   region         = "us-central1"
 }
-

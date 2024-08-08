99b2ca4d3ebada4579af8e69b2478d0ecd01cdce
Zhyldyz Baiseitova <baiseitovazhyldyz4@gmail.com>
2024-08-08 07:40:24 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
-module "vpc" {
-  source       = "./modules/vpc"
-  region       = var.region
-  subnet_cidrs = var.subnet_cidrs
-  project_id   = var.project_id
+terraform {
+  backend "gcs" {
+    bucket  = "my-terraform-state-bucket"  # Имя GCS bucket
+    prefix  = "terraform/state"            # Путь внутри bucket, где будет храниться состояние
+  }
 }
+
+provider "google" {
+  credentials = file("C:\Users\jikab\Desktop\CICD\gitlab-cicd-431608-b129d9fbffc9.json")  # Путь к файлу с ключом сервисного аккаунта
+  project     = "gitlab-cicd-431608"                           # ID проекта в GCP
+  region      = "us-central1"                                   
+}
+
+# Пример ресурсов
+resource "google_compute_network" "default" {
+  name = "default-network"
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "default-subnetwork"
+  ip_cidr_range  = "10.0.0.0/24"
+  network        = google_compute_network.default.id
+  region         = "us-central1"
+}
+
